2d2ef9f97fd8f658c98391ac254bf653a15af57d
nu-jey <yp50777@naver.com>
2023-07-15 19:40:17 +0900

    Initial Commit

---
+++ Financial Freedom/Financial_FreedomApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_FreedomApp.swift
+//  Financial Freedom
+//
+//  Created by 오예준 on 2023/07/15.
+//
+
+import SwiftUI
+
+@main
+struct Financial_FreedomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
