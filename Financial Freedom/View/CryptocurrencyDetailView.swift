963e0c85cb122b0f8d4891ed84c2580915f478f5
nu-jey <yp50777@naver.com>
2023-07-18 22:39:37 +0900

    - 피커&차트 오류 해결 ->  피커를 통해 세그먼트 값 변경시 onChange를 통해서 차트 리로딩 - 상단에 tradePrice와 추세 이미지 추가 -> 추후 추세관련 알고리즘 작성 후 적용

--- Financial Freedom/View/CryptocurrencyDetailView.swift
+++ Financial Freedom/View/CryptocurrencyDetailView.swift
@@ -8,43 +8,28 @@
 import SwiftUI
 import UpbitSwift
 import Charts
-extension CandleType: Hashable {
-    public static func == (lhs: CandleType, rhs: CandleType) -> Bool {
-        return lhs.hashValue == rhs.hashValue
-    }
-    
-    public func hash(into hasher: inout Hasher) {
-        switch self {
-        case .minute(let value):
-            hasher.combine(value)
-        case .hour(let value):
-            hasher.combine(value)
-        case .days:
-            break
-        case .weeks:
-            break
-        case .months:
-            break
-        }
-    }
-    
-    
-}
+
 struct CryptocurrencyDetailView: View {
-    @State var currentTicker: String?
+    @State var currentTicker:String
     @State var segmentationSelection: CandleType = .minute(.fifteen )
-    @State var range: Int = 10
+    @State var range: Int = 100
     @StateObject var cryptocurrencyDetailViewModel = CryptocurrencyDetailViewModel()
+    
     var body: some View {
         ScrollView {
             VStack {
                 HStack() {
-                    Text(currentTicker!)
+                    Text(currentTicker)
                         .font(.title)
                         .padding(.leading, 20)
+                    Image(systemName: cryptocurrencyDetailViewModel.chartState.image)
                     Spacer()
+                    Text("\(cryptocurrencyDetailViewModel.tickers.map { $0.tradePrice}.reduce(0, +))")
+                        .padding(.trailing, 20)
                 }
-                Divider()
+                .onAppear {
+                    cryptocurrencyDetailViewModel.searchTicker(targetMarket: currentTicker)
+                }
                 // 요약 정보 뷰
                 Divider()
                 Chart() {
@@ -58,17 +43,14 @@ struct CryptocurrencyDetailView: View {
                 }
                 .chartYScale(domain:cryptocurrencyDetailViewModel.chartRange.0...cryptocurrencyDetailViewModel.chartRange.1)
                 .frame(height:  UIScreen.main.bounds.width / 2)
-                Picker("Candle", selection: $segmentationSelection.animation(.easeInOut)) {
+                .onChange(of: self.segmentationSelection) { changeCandleType in
+                        cryptocurrencyDetailViewModel.searchCandle(targetMarket: currentTicker, candleType: changeCandleType, range: range)
+                }
+                Picker("Candle", selection:$segmentationSelection) {
                     Text("minutes")
                         .tag(CandleType.minute(.fifteen))
-                        .onTapGesture {
-                            print(segmentationSelection)
-                        }
                     Text("hours")
                         .tag(CandleType.hour(.four))
-                        .onTapGesture {
-                            print(segmentationSelection)
-                        }
                     Text("days")
                         .tag(CandleType.days)
                     Text("weeks")
@@ -82,9 +64,11 @@ struct CryptocurrencyDetailView: View {
                 
             }
             .onAppear() {
-                cryptocurrencyDetailViewModel.searchCandle(targetMarket: currentTicker!, candleType: segmentationSelection, range: 100)
+                cryptocurrencyDetailViewModel.searchCandle(targetMarket: currentTicker, candleType: segmentationSelection, range: range)
+                cryptocurrencyDetailViewModel.searchTicker(targetMarket: currentTicker)
             }
         }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height-100)
     }
 }
 
--- Financial Freedom/View/CryptocurrencyView.swift
+++ Financial Freedom/View/CryptocurrencyView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct CryptocurrencyView: View {
     @StateObject var cryptocurrencyViewModel = CryptocurrencyViewModel()
     var body: some View {
-        
         List(cryptocurrencyViewModel.marketList.filter({ $0.market.contains("KRW-") }), id: \.market) { market in
-            NavigationLink(destination: CryptocurrencyDetailView(currentTicker: market.market)) {
+            NavigationLink(destination:CryptocurrencyDetailView(currentTicker: market.market)) {
                 Text(market.koreanName)
             }
         }
